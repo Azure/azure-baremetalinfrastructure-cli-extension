d14b71792bca315dc66aae3bb2697db01c87bee0
Page Bowers <pabowers@microsoft.com>
2020-09-10 14:25:57 -0700

    filling out the help menu

--- azext_baremetalinfrastructure/_help.py
+++ azext_baremetalinfrastructure/_help.py
@@ -11,6 +11,7 @@ helps['azurebaremetalinstance'] = """
     type: group
     short-summary: (PREVIEW) Manage Azure Bare Metal Instance.
     """
+
 helps['azurebaremetalinstance show'] = """
     type: command
     short-summary: Get the details of an Azure Bare Metal Instance.
@@ -19,4 +20,29 @@ helps['azurebaremetalinstance show'] = """
 helps['azurebaremetalinstance list'] = """
     type: command
     short-summary: List Azure Bare Metal Instances.
+"""
+
+helps['azurebaremetalinstance restart'] = """
+    type: command
+    short-summary: Restart an Azure Bare Metal Instance.
+"""
+
+helps['azurebaremetalinstance start'] = """
+    type: command
+    short-summary: Start an Azure Bare Metal Instance.
+"""
+
+helps['azurebaremetalinstance shutdown'] = """
+    type: command
+    short-summary: Shutdown an Azure Bare Metal Instance.
+"""
+
+helps['azurebaremetalinstance update'] = """
+    type: command
+    short-summary: Update the tags field of an Azure Bare Metal Instance.
+"""
+
+helps['azurebaremetalinstance delete'] = """
+    type: command
+    short-summary: Delete an Azure Bare Metal Instance.
 """
